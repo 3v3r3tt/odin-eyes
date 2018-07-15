c546fabc7d2c5e8a2d800e249175094de18094a7
Paul M <paul@deyrd.com>
2018-07-15 14:28:40 -0600

    Change back to app.css

--- src/EntryPage/EntryPage.js
+++ src/EntryPage/EntryPage.js
@@ -7,7 +7,7 @@ import { ForgotPasswordForm } from './ForgotPasswordForm'
 import { Brand } from './Brand'
 import { Thanks } from './Thanks'
 
-import '../app.scss'
+import '../app.css'
 import logo from '../_assets/images/1x/logo-rectangle.png'
 
 import { userActions } from '../_actions'
